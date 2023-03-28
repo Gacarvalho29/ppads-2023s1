10715e600c413e740f3f6c81559b8b1b37b5abdb
Gacarvalho29 <Gabrielbco25@gmail.com>
2023-03-28 15:25:56 -0300

    add SSL

--- src/siteback/src/main.ts
+++ src/siteback/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{
+    httpsOptions: {
+      key: readFileSync(resolve(__dirname, "../cert/key.pem")),
+      cert: readFileSync(resolve(__dirname, "../cert/cert.pem"))
+    },
+  });
   app.enableCors()
 
-  await app.listen(3000);
+  await app.listen(443);
 }
 bootstrap();
