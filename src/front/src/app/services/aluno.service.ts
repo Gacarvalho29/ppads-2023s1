c92b547a878ce4070ec3ebfdf3c0edb12e2a6373
Gacarvalho29 <Gabrielbco25@gmail.com>
2023-03-28 12:45:59 -0300

    troca de porta do backend

--- src/front/src/app/services/aluno.service.ts
+++ src/front/src/app/services/aluno.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class AlunoService {
 
-  BASE_URL: string = 'http://localhost:3000';
+  BASE_URL: string = 'http://18.229.143.132';
 
   constructor(private http: HttpClient) { }
 
